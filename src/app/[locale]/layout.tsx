b280b5d5953ea08a5b1de97d3b88334c98968776
plumbus <plum.bus@yahoo.com>
2026-08-12 11:04:12 +0700

    chore: added favicon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -33,6 +33,14 @@ export async function generateMetadata({ params }: LayoutProps): Promise<Metadat
     metadataBase: new URL(site.url),
     title: { default: t("title"), template: `%s — ${site.name}` },
     description: t("description"),
+    icons: {
+      icon: [
+        { url: "favicon-96x96.png", type: "image/png", sizes: "96x96" },
+        { url: "favicon.svg", type: "image/svg+xml" },
+      ],
+      shortcut: "favicon.ico",
+      apple: [{ url: "apple-touch-icon.png", sizes: "180x180" }],
+    },
   };
 }
 
